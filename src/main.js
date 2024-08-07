f014df0770284d57b19b7cdf7700f1a38f1c4b95
Zhiyuan Xu <william@zerodots.net>
2024-08-07 20:33:33 +0800

    基础项目完成

---
+++ src/main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow(){
+    const mainWindow = new BrowserWindow({
+        width:1280,
+        height:960,
+        icon:path.join(__dirname, '.', 'asset', 'icon.png')
+    })
+
+    mainWindow.loadURL('http://server.zerodots.net:8123/')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit()
+})
